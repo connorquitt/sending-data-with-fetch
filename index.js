210c97db1c0ec403aaa2d0fc68fa40703d3017dd
connorq <connorquitt@gmail.com>
2023-08-23 21:59:53 -0500

    all tests pass

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Add your code here
+function submitData(name, email) {
+    const userInfo = {
+        name,
+        email
+    }
+    return fetch(`http://localhost:3000/users`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify(userInfo)
+    })
+    .then((resp) => resp.json())
+    .then((data) => (document.body.innerHTML = data['id']))
+    .catch((error) => {
+        document.body.innerHTML = error.message
+    })
+    
+}
+
+console.log(submitData("hi", "hi"))
